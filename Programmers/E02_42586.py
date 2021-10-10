a59363a36971099ad76474ddb2f58dc8d4f4e39c
pineplanet <ggul_jam@icloud.com>
2021-10-10 18:23:30 +0900

    programmers algorithm

---
+++ Programmers/E02_42586.py
@@ -0,0 +1,32 @@
+import math
+from collections import deque
+
+
+def solution(progresses, speeds):
+    answer = []
+    due = deque()
+    progresses = deque(progresses)
+    for i in range(len(progresses)):
+        due.append(math.ceil((100 - progresses[i]) / speeds[i]))
+    print(due)
+
+    work = due.popleft()
+    cnt = 1
+    while due:
+        if work >= due[0]:
+            cnt = cnt +1
+            due.popleft()
+        elif work < due[0]:
+            answer.append(cnt)
+            print(work, cnt)
+
+            work = due.popleft()
+            cnt = 1
+    answer.append(cnt)
+    print (work,cnt)
+    return answer
+
+
+
+#solution([93, 30, 55], [1, 30, 5])
+solution([95, 90, 99, 99, 80, 99],[1, 1, 1, 1, 1, 1])
